b44735ca530ed18133db4dea53ff1553d7f4305f
Yonathan <yonathan.piat@gmail.com>
2021-10-17 13:44:43 +0200

    ajoute de findone

--- controllers/user.js
+++ controllers/user.js
@@ -15,5 +15,18 @@ module.exports = {
         })
             .then(user => res.status(201).send(user))
             .catch(error => res.status(400).send(error))
+    },
+    findOne(req, res) {
+        const id = req.params.id;
+        return User.findByPk(id)
+            .then(data => {
+                res.send(data);
+            })
+            .catch(err => {
+                res.status(500).send({
+                    message: "Erreur lors de la récupération du didacticiel avec id=" + id
+                });
+            })
     }
+
 };
